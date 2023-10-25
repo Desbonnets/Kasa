1b7810a58fc15daf58900e3f40286af546bb22a4
Desbonnets <gillesdesbonnets@gmail.com>
2023-10-25 16:29:37 +0200

    fix logo

--- src/route/App.js
+++ src/route/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/App.css';
 import { Outlet } from 'react-router-dom';
 import Image from '../Composant/Image';
 import Lien from '../Composant/Lien';
 
 function Header(){
+  const [imageSrc, setImageSrc] = useState('');
+
+  useEffect(() => {
+    // Fonction pour déterminer la largeur de l'écran et choisir le lien de l'image en conséquence
+    const updateImageSrc = () => {
+      const screenWidth = window.innerWidth;
+
+      if (screenWidth >= 768) {
+        setImageSrc(`../images/logo_color.svg`);
+      } else {
+        setImageSrc(`../images/logo_color_mobil.svg`);
+      }
+    };
+
+    // Appeler la fonction lors du chargement initial et lorsque la fenêtre est redimensionnée
+    window.addEventListener('resize', updateImageSrc);
+    updateImageSrc();
+
+    // Nettoyer l'écouteur d'événements lors du démontage du composant
+    return () => {
+      window.removeEventListener('resize', updateImageSrc);
+    };
+  }, []);
   return  <header className='page-header taille-content'>
-            <Image src={`../images/logo_color${window.innerWidth <= 768 ? '_mobil': ''}.svg`} alt='Logo'/>
+            <Image src={imageSrc} alt='Logo'/>
             <nav><Lien to='/' text='Accueil'/><Lien to='/A_Propos' text='A Propos'/></nav>
           </header>
 }
